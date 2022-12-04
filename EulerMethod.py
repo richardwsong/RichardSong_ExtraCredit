c94dcca472a0a93ba8593746157ad8deb5f2a758
Richard Song <richardwsong@gmail.com>
2022-12-04 16:09:31 -0600

    Added comments

--- EulerMethod.py
+++ EulerMethod.py
@@ -1,4 +1,26 @@
-# Uses Euler's Method to approximate and plot the value of p
+""" 
+This code sses Euler's Method to approximate and plot the value of p. 
+It starts by importing the proper libraries, matplotlib.pyploy (which can create
+scatter plots when given a list of x and a list of y values) and numpy (which will
+turn regular arrays into numpy arrays for matplotlib). We initialize a vector of 
+length 40 called x and y, which will represent our points across 40 time points that
+we calculate using Euler's method. Our step size, h, can be arbitrarily specified.
+In our problem, we used h values of 0.18, 0.23, 0.25, and 0.3 to estimate the
+function p (in our case, the y coordinate). Next, we create a for loop going from 1 
+to 40. From part b, we determined that p_(n+1) = (1+10h)*p_n - (10h)*(p_n)^2. We 
+will use this equation to calculate individual values of y in the for loop. Each 
+value of x will be step count, i, multiplied by the step size, h. Finally, we create
+numpy verisons of the x and y arrays, and display the scatter plot. 
+
+Interestingly, for low step sizes (i.e., h = 0.18), the value of p will approach 1
+as x approaches infinity. We verified this to be true from part a, since the limit of
+p as x approaches infinity is 1. However, our Euler's Identity estimation will 
+exhibit more and more chaos as the value of h increases. For example, for h = 0.23, 
+p alternates between 1.18 and 0.69 as x increases (i.e., it oscillates between 2 
+different points). For h = 0.25, p jumps between 1.23, 0.54, 1.16, and 0.7 as x 
+increases (i.e., it osccilates between 4 different points). And for h = 0.3, there is
+no observable pattern for the oscillation activity of p as x increases. 
+"""
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,7 +34,7 @@ h = 0.30
 
 for i in range(1, 40):
     y[i] = y[i-1]*(1+10*h) - (10*h)*y[i-1]*y[i-1]
-    x[i] = .18*i
+    x[i] = h*i
 
 x_np = np.array(x)
 y_np = np.array(y)
--- RungeKutta.py
+++ RungeKutta.py
@@ -1,5 +1,31 @@
-# Uses Runge-Kutta Method to approximate and plot the value of p
+"""
+This script uses Runge-Kutta Method to approximate and plot the value of p. The
+Runge-Kutta method is descirbed in Section 5.7 of the textbook, and rescursively
+calculates the value of x and y based on intermediate claculations of the values 
+k1, k2, k3, and k4. Although the Runge-Kutta Method is fairly accurate with smaller
+step sizes, several errors can arise when the step size is too large. The values of 
+h that are displayed with this problem demonstrate some of those errors. We used
+h = 0.3, 0.325, and 0.35 for this problem. For the largest value h = 0.35, we saw
+that the value of p tended to oscillate between 0.51 and 0.72. Lowering the value 
+of h to 0.325 solved the oscillating problem, however p ended up converging to 
+0.67 as x increased, even though it should have convered to 1. Lowering the value of h
+again to 0.3 caused p to converge to 0.76, which is closer to the actual answer. 
+Interestingly, lowering the value of p to 0.2 causes it to converge to the correct 
+answer (p = 1) as x increases. 
 
+The way that this script works is quite similar to the EulerMethod.py script. We 
+first import the necessary libraries for plotting, namely matplotlib.pyplot and 
+numpy. We then define a function f that represents the value of dp/dt for 
+code-writing simplicity. We then intitialze the arrays x and y, which are the 
+coordinates of our eventual scatter plot, to size 60 because we have 60 step counts. 
+The variable h will represent our step size, which in this case are 0.3, 0.325, and 
+0.35. To recursively calculate the values of x and y, we create a for loop that loops
+from 1 to 60 that will update k1, k2, k3, and k4 based on the value of the function
+f at the previous step and the step count. The values of k1, k2, k3, and k4 at the
+current iteration will then be used to update the values of x and y for the current
+iteration. Finally, we convert and x and y arrays to numpy arrays and plot them using
+matplotlib. 
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 
